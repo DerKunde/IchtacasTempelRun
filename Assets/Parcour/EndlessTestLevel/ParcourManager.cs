589768224a5c284589b3bf5053d38c218405ca48
DerKunde <sean.rindl@stud.hshl.de>
2024-11-03 19:29:26 +0100

    TestiCommitiUser

--- Assets/Parcour/EndlessTestLevel/ParcourManager.cs
+++ Assets/Parcour/EndlessTestLevel/ParcourManager.cs
@@ -7,7 +7,7 @@ public class ParcourManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ///
     }
 
     // Update is called once per frame
